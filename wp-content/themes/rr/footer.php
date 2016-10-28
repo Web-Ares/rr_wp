a71bb3209b65656e4b8e6f969ba9ffb9195fc52b
aleksandrnikolajcuk <deathstars111>
2016-10-28 14:08:15 +0300

    Form MAster header menu

--- wp-content/themes/rr/footer.php
+++ wp-content/themes/rr/footer.php
@@ -11,63 +11,7 @@
         <!-- popup__content -->
         <div class="popup__content popup__request">
 
-            <form method="post" enctype="multipart/form-data" id="gform_1" action="/">
-                <div class="gform_heading">
-                    <h3 class="gform_title">REQUEST A QUOTE</h3>
-                    <span class="gform_description">Tell us more about your project - e.g. expected monthly volumes and special requirements and we will get back to you with the best offer.</span>
-                </div>
-                <div class="validation_error">There was a problem with your submission. Errors have been highlighted below.</div>
-                <div class="gform_body">
-                    <ul id="gform_fields_1" class="gform_fields top_label form_sublabel_below description_below">
-                        <li id="field_1_3" class="gfield gfield_error field_sublabel_below field_description_below">
-                            <label class="gfield_label gfield_label_before_complex" for="input_1_3_3">YOUR NAME*</label>
-                            <div class="ginput_complex ginput_container no_prefix has_first_name no_middle_name no_last_name no_suffix gf_name_has_1 ginput_container_name gfield_trigger_change" id="input_1_3">
-
-                                <!--<span id="input_1_3_3_container" class="name_first">-->
-                                <input type="text" name="input_3.3" id="input_1_3_3" value="" aria-label="First name" tabindex="2" aria-invalid="true">
-                                <!--<label for="input_1_3_3">First</label>-->
-                                <!--</span>-->
-
-                            </div>
-                            <div class="gfield_description validation_message">At least one field must be filled out</div>
-                        </li>
-                        <li id="field_1_2" class="gfield gfield_error field_sublabel_below field_description_below">
-                            <label class="gfield_label" for="input_1_2">E-MAIL*</label><div class="ginput_container ginput_container_email">
-                                <input name="input_2" id="input_1_2" type="text" value="" class="medium" tabindex="6">
-                            </div>
-                            <div class="gfield_description validation_message">At least one field must be filled out</div>
-                        </li>
-                        <li id="field_1_4" class="gfield gfield_error field_sublabel_below field_description_below">
-                            <label class="gfield_label" for="input_1_4">PHONE</label>
-                            <div class="ginput_container ginput_container_phone">
-                                <input name="input_4" id="input_1_4" type="text" value="" class="medium" tabindex="7" aria-invalid="true">
-                                <div class="instruction validation_message">Phone format: (###) ###-####</div>
-                            </div>
-                            <div class="gfield_description validation_message">At least one field must be filled out</div>
-                        </li>
-                        <li id="field_1_5" class="gfield gfield_error field_sublabel_below field_description_below">
-                            <label class="gfield_label" for="input_1_5">DETAILS*</label>
-                            <div class="ginput_container">
-                                <textarea name="input_5" id="input_1_5" class="textarea medium" tabindex="8" aria-invalid="true" rows="10" cols="50"></textarea>
-                            </div>
-                            <div class="gfield_description validation_message">At least one field must be filled out</div>
-                        </li>
-                        <li id="field_1_7" class="gfield gfield_html gfield_html_formatted gfield_no_follows_desc field_sublabel_below field_description_below"><div class="g-recaptcha" data-sitekey="6LfTbwoUAAAAAI8pE-5iniFGkysjT5QeYqAb6WwI"><div style="width: 304px; height: 78px;"><div><iframe src="https://www.google.com/recaptcha/api2/anchor?k=6LfTbwoUAAAAAI8pE-5iniFGkysjT5QeYqAb6WwI&amp;co=aHR0cDovL3JyX3dwOjgw&amp;hl=ru&amp;v=r20161018160839&amp;size=normal&amp;cb=64wn7vfikn55" title="виджет reCAPTCHA" width="304" height="78" role="presentation" frameborder="0" scrolling="no" name="undefined"></iframe></div><textarea id="g-recaptcha-response" name="g-recaptcha-response" class="g-recaptcha-response" style="width: 250px; height: 40px; border: 1px solid #c1c1c1; margin: 10px 25px; padding: 0px; resize: none;  display: none; "></textarea></div></div></li>
-                    </ul>
-                </div>
-                <div class="gform_footer top_label">
-                    <button class="button btn btn_4" id="gform_submit_button_1">
-                        <span>PLACE ORDER</span>
-                    </button>
-                    <input type="hidden" class="gform_hidden" name="is_submit_1" value="1">
-                    <input type="hidden" class="gform_hidden" name="gform_submit" value="1">
-                    <input type="hidden" class="gform_hidden" name="gform_unique_id" value="5811edf58c269">
-                    <input type="hidden" class="gform_hidden" name="state_1" value="WyJbXSIsImVmMjYxMDczZDgyMjYxMTcyOTAyNDc2MzlhNGNmYWViIl0=">
-                    <input type="hidden" class="gform_hidden" name="gform_target_page_number_1" id="gform_target_page_number_1" value="0">
-                    <input type="hidden" class="gform_hidden" name="gform_source_page_number_1" id="gform_source_page_number_1" value="1">
-                    <input type="hidden" name="gform_field_values" value="">
-                </div>
-            </form>
+            <?= do_shortcode('[gravityform id=2 title=true description=true ajax=true]') ?>
 
         </div>
         <!-- /popup__content -->
