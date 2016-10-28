f3b41c64b56284065ee66f28b8ad077f7ade016b
aleksandrnikolajcuk <deathstars111>
2016-10-28 14:54:24 +0300

    Form MAster header menu

--- wp-content/themes/rr/header.php
+++ wp-content/themes/rr/header.php
@@ -7,6 +7,7 @@
     <meta name="format-detection" content="address=no">
 
     <title><?php wp_title(''); ?></title>
+    <?php gravity_form_enqueue_scripts( 2, true ); ?>
     <?php wp_head(); ?>
   
 
